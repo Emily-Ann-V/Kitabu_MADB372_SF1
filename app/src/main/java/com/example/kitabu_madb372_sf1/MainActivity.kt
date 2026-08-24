ad0a2e03823d82cd8a3f801e23c14d62dd224da7
Emily-Ann V <emilyann.vdwalt@gmail.com>
2026-08-24 12:09:57 +0200

    Add basic Book Catalog Screen layout

--- app/src/main/java/com/example/kitabu_madb372_sf1/MainActivity.kt
+++ app/src/main/java/com/example/kitabu_madb372_sf1/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.kitabu_madb372_sf1
 
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,16 +16,22 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.kitabu_madb372_sf1.ui.theme.Kitabu_MADB372_SF1Theme
@@ -50,6 +58,16 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 
     Column(modifier = Modifier.fillMaxSize()) {
+        //Header
+        Text(
+            text = "Our Catalog",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            textAlign = TextAlign.Center
+        )
+
+        // Search
         Row(
             modifier = Modifier
                 .fillMaxWidth()
@@ -61,6 +79,13 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 onValueChange = {},
                 placeholder = {
                     Text("Search books")
+                },
+                // leadingIcon - Puts icon on the right
+                trailingIcon = { // Putting icon on the left
+                    Icon(
+                        imageVector = Icons.Default.Search,// Need to add dependency
+                        contentDescription = "Search"
+                    )
                 }
             )
 
@@ -71,11 +96,12 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             }
         }
 
-
+        //Books
         LazyVerticalGrid(
             // Scrolls vertically
             columns = GridCells.Adaptive(minSize = 130.dp), // Adaptive cell width based on screen size
             modifier = Modifier
+                .weight(1f) // Filling remaining middle space
                 .padding(20.dp, 50.dp), // Adding padding to the whole grid
             horizontalArrangement = Arrangement.spacedBy(20.dp), // Adding horizontal padding to in-between items
             verticalArrangement = Arrangement.spacedBy(20.dp) // Adding vertical padding to in-between items
@@ -101,7 +127,13 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                             modifier = Modifier.fillMaxWidth(), // Making the column the full card's width
                             horizontalAlignment = Alignment.CenterHorizontally // Horizontally centering the text
                         ) {
-                            //TODO: Image
+                            // Cover
+                            Image(
+                                painter = painterResource(R.drawable.img_default),
+                                contentDescription = "Book cover"
+                            )
+
+                            // Details
                             Text(text = "Title")
                             Text(text = "Author")
                             Text(text = "Category")
@@ -129,6 +161,13 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                             modifier = Modifier.fillMaxWidth(),
                             horizontalAlignment = Alignment.CenterHorizontally
                         ) {
+                            // Cover
+                            Image(
+                                painter = painterResource(R.drawable.img_default),
+                                contentDescription = "Book cover"
+                            )
+
+                            // Details
                             Text(text = "Title")
                             Text(text = "Author")
                             Text(text = "Category")
@@ -136,6 +175,23 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                     }
                 }
             }
+        }
+
+        // Footer
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            horizontalArrangement = Arrangement.End
+        ) {
+            Text(
+                text = "Your Reservations"
+            )
+
+            Icon(
+                painter = painterResource(R.drawable.ic_arrow_forward),
+                contentDescription = "Next Page"
+            )
         }
     }
 }
